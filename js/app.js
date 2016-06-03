2f59e8e3fedffa0cba3a6a558cc4aaf1cab4767e
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-03 08:56:14 +0200

    add project

--- js/app.js
+++ js/app.js
@@ -3,10 +3,21 @@ $(document).ready(function() {
         anchors:['home', 'about', 'work', 'contact'],
         keyboardScrolling: false
     });
-    $('.slider').slick({ dots : true,
-    	arrows : false})
+
+    $('.fade').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 700,
+		  cssEase: 'linear',
+		  arrows : false,
+		  autoplay : true,
+		  autoplaySpeed : 2000,
+		  fade : true
+		});
+
     $(".btn-toggle-menu").click(function(){
     	$(".main-menu").toggleClass("hide-menu");
     })
 });
+
 
